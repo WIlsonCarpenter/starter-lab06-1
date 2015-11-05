3fdb2e4ae8260f26a71b4e3de9492e21f3549278
Wilson Carpenter <wilsonkcarpenter@gmail.com>
2015-11-05 15:15:52 -0800

    Created a dtd file for order validation. Created 3 new burger orders. Orders validated.

---
+++ data/order3.php
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<!DOCTYPE order SYSTEM 'bonanza.dtd'>
+
+<order type="delivery">
+    <customer>Wilson</customer>
+    <burger>
+        <patty type="bison"/>
+        <cheeses top="jack" bottom="swiss"/>
+        <topping type="shrooms"/>
+        <topping type="tomato"/>
+        <topping type="bacon"/>
+        <topping type="raw"/>
+        <topping type="roasted"/>
+        <topping type="egg"/>
+        <sauce type="mustard"/>
+        <sauce type="pb"/>
+        <instructions>Easy on the peanut butter. I don't like it but 
+        			  I'm being forced to order it.</instructions>
+    </burger>
+</order>
